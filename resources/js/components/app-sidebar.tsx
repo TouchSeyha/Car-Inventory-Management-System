3b1099ad1da9735af9d83fb0a113fcf209df9e01
TouchSeyha <hacurly13@gmail.com>
2025-05-02 16:34:55 +0700

    Refactor: streamline icon imports in AppSidebar for cleaner code

--- resources/js/components/app-sidebar.tsx
+++ resources/js/components/app-sidebar.tsx
@@ -4,15 +4,7 @@ import { NavUser } from '@/components/nav-user';
 import { Sidebar, SidebarContent, SidebarFooter, SidebarHeader, SidebarMenu, SidebarMenuButton, SidebarMenuItem } from '@/components/ui/sidebar';
 import { type NavItem } from '@/types';
 import { Link } from '@inertiajs/react';
-import {
-    BookOpen,
-    Folder,
-    ShoppingCart,
-    LayoutDashboard,
-    Car,
-    ClipboardList,
-    Users,
-} from 'lucide-react';
+import { BookOpen, Car, ClipboardList, Folder, LayoutDashboard, ShoppingCart, Users } from 'lucide-react';
 import AppLogo from './app-logo';
 
 const mainNavItems: NavItem[] = [
